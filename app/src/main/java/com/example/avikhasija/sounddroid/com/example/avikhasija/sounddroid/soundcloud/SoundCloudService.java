12cd4a83dd12f51a1f39fa19e4b987ffe0b10127
AvikHasija <avikhasija@gmail.com>
2015-07-20 23:35:22 -0400

    Final Commit

--- app/src/main/java/com/example/avikhasija/sounddroid/com/example/avikhasija/sounddroid/soundcloud/SoundCloudService.java
+++ app/src/main/java/com/example/avikhasija/sounddroid/com/example/avikhasija/sounddroid/soundcloud/SoundCloudService.java
@@ -10,7 +10,8 @@ import retrofit.http.Query;
  * Created by Avikshit on 7/19/2015.
  */
 public interface SoundCloudService {
-    
+
+    static final String CLIENT_ID = "PLACEHOLDER";
 
     @GET("/tracks?client_id="+CLIENT_ID)
     public void searchSongs(@Query("q") String query, Callback<List<Track>> cb);
